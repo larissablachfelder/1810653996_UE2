6f9e69e833813b0dbf000d9f242aef4cf55120df
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-26 15:01:49 +0100

    aufgabe 2

---
+++ src/Uebung2/Main.java
@@ -0,0 +1,51 @@
+package Uebung2;
+
+import javax.swing.*;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        //Hier werden zwei zahlen vom benutzer eingelesen
+        int eingabe1 = Integer.parseInt(JOptionPane.showInputDialog("Bitte geben Sie eine Zahl ein!"));
+        int eingabe2 = Integer.parseInt(JOptionPane.showInputDialog("Bitte geben Sie eine Zahl ein! Die Zahlen werden jetzt überprüft."));
+
+        //die Zahlen werden überprüft
+        if(eingabe1==eingabe2)//wenn die Zahlen gleich sind, werden diese ausgegeben
+        {
+           JOptionPane.showMessageDialog(null, "Die Zahlen stimmen überein. Zahl 1: "+eingabe1 + ", Zahl 2: " + eingabe2);
+           break;
+        }
+
+        else //wenn die Zahlen nicht gleich sind
+        {
+            for(int i =0; ; i++)//schleife; der mittlere teil bleibt frei, da wir keine höchstgrenze wissen
+            {
+
+
+                if(eingabe1<eingabe2)//wenn zahl 1 kleiner ist, wird diese erhöht
+                {
+                    eingabe1++;
+                    JOptionPane.showMessageDialog(null, "Die Zahl1 wurde um 1 erhöht.");
+                }
+
+                else //wenn zahl 2 kleiner ist wird diese erhöht
+                {
+                    eingabe2++;
+                    JOptionPane.showMessageDialog(null, "Die Zahl2 wurde um 1 erhöht");
+                }
+
+                if(eingabe1==eingabe2)//wenn die Zahlen jetzt übereinstimmen wird die anzahl der durchgänge ausgegeben
+                {
+                    JOptionPane.showMessageDialog(null, "Die Zahlen stimmen jetzt nach " + i + ". Durchläufen überein.");
+                    break;
+                }
+
+
+            }
+        }
+
+    }
+}
